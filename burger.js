832571f7682ac3a11737f8c698405470628c5a66
samuel giraldo <samueldavidgiraldogutierrez@gmail.com>
2023-08-16 12:36:04 -0500

    Mi portafolio

--- burger.js
+++ burger.js
@@ -2,6 +2,8 @@ const burgerBtn = document.querySelector('.burger-btn')
 const closeBtn = document.querySelector('.close-btn')
 const head = document.querySelector('head')
 const navBurger = document.querySelector('.nav-burger')
+const main = document.querySelector('.main')
+const header = document.querySelector('header')
 
 
 burgerBtn.addEventListener('click' , () =>{
@@ -14,3 +16,6 @@ closeBtn.addEventListener('click' , () => {
    navBurger.classList.remove('nav-burger-visible')
 })
 
+main.addEventListener('click' , () => {
+    navBurger.classList.remove('nav-burger-visible')
+})
